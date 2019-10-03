77a5cc0d07ca4aa423af5fa4d8646ec641830149
PTGM <develop@ptgm.info>
2019-10-04 01:59:20 +0900

    銘柄データをWindowsにコピーするようにした

--- mkenv.sh
+++ mkenv.sh
@@ -3,6 +3,7 @@
 # Python実行環境の構築
 PYTHON=/usr/bin/python3
 COMMON_DIR=~/hostpg
+MEIGAR_CSV=us-meigara.csv
 
 if [ -e yfkenv ]; then
     rm -rf ~/yfkenv
@@ -10,9 +11,16 @@ fi
 
 mkdir ~/yfkenv
 
+# install Python
 cd ~/yfkenv
 $PYTHON -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install wheel
 pip install numpy pandas yfinance
+
+# make directories in Windows
+mkdir $COMMON_DIR/yfk_{csv,meigara,bat}
+
+SCR_DIR=$(cd $(dirname $0); pwd)
+cp $COMMON_DIR/yfk_meigara/$MEIGARA_CSV
